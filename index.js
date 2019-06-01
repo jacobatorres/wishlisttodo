d80693fbd06fcd7a60142367ef462bc8928e84d1
Jacob Torres <jacobangelo_torres@yahoo.com>
2019-05-31 22:45:10 -0700

    Add Mongoose, setup MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express"),
 app = express(),
 // debug = require("debug"),
 bodyparser = require("body-parser"),
+mongoose = require("mongoose"),
 hostname = '127.0.0.1',
 port = 3000;
 
@@ -9,6 +10,33 @@ port = 3000;
 // most necessary
 app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/wishlist_app', {useNewUrlParser: true});
+
+// schema for the list
+var listSchema = new mongoose.Schema({
+	name: String,
+	description: String
+});
+
+// compile to a model
+var List = mongoose.model("List", listSchema);
+
+// // sample list
+// List.create(
+// 	{
+// 		name: "Trip to Europe",
+// 		description: "Make sure I dont forget these things:"
+// 	}, function(err, campground){
+// 		if (err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("new campground!!!");
+// 			console.log(campground);
+// 		}
+// });
+
+
+
 
 /// routes
 
@@ -19,7 +47,16 @@ app.get("/", function(req, res){
 
 // index route
 app.get("/lists", function(req, res){
-	res.render("index");
+
+	// find all campgrounds
+	List.find({}, function(err, all_lists){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index", {lists:all_lists});
+		}
+	});
+
 });
 
 
@@ -32,11 +69,20 @@ app.get("/lists/new", function(req, res){
 // create route 
 app.post("/", function(req, res){
 
-	const return_object = {
-		title: req.body.title, 
+	const list_to_add = {
+		name: req.body.name, 
 		description: req.body.description
 	};
-	res.render("temppost", {object: return_object});
+
+
+	List.create(list_to_add, function(err, newlist){
+		if(err){
+			console.log("err");
+		} else {
+			// successfully added list! go back to index
+			res.redirect("/lists")
+		}
+	});
 
 });
 
