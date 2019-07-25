46fcbb697331964124e3deb5ec315114413c563d
Jacob Torres <jacobangelo_torres@yahoo.com>
2019-07-25 13:06:04 -0700

    Save item when hitting enter

--- routes/items.js
+++ routes/items.js
@@ -1,23 +1,39 @@
 var express = require("express");
 var router = express.Router();
 var passport = require("passport");
-var User = require("../models/user");
-var List = require("../models/list");
+var Item = require("../models/item");
+
 // var app = express.createServer();
 
 // app.use(express.bodyParser());
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var middleware = require("../middleware");
-
 
 // when user hits Enter in show > Add new Item, this route will be used
 router.post("/lists/:list_id/items", function(req, res){
 
 	console.log("yeah baby");
 
-	console.log(req);
+	var obj = {};
+	console.log('body: ' + JSON.stringify(req.body));
 
-	res.redirect("/lists/" + req.params.list_id);
+	Item.create(req.body, function(err, list){
+
+		if (err){
+			console.log("error", "Error creating the list");
+			return res.redirect("back");
+		} 
+
+		// if successful, flash success and see show page
+		res.redirect('/lists/' + req.params.list_id);
+
+
+
+	})
+
 
 });
 
